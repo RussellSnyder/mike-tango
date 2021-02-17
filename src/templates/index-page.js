3ec0ad7d5d2d135db536b82b2b245174ec71aa55
Russell Snyder <russell.snyder@methodpark.de>
2021-02-17 17:35:08 +0100

    add musicroll import

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,7 +4,7 @@ import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import MusicRoll from '../components/MusicRoll'
 
 export const IndexPageTemplate = ({
   image,
